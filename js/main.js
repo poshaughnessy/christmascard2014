ab1193a3171b8b53719889d8f293d4eb4e9d6587
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2014-12-22 13:36:44 +0000

    Trying to fix scaling for resizing window

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-var stage,
-    renderer,
+var SANTA_SIZE = 250,
+    SANTA_PADDING = 10,
+    DIRECTION = {LEFT : 0, RIGHT : 1},
+    MINCE_PIE_WIDTH = 114,
+    MINCE_PIE_HEIGHT = 72,
+    stage = new PIXI.Stage(0x55813a),
+    gameContainer = new PIXI.DisplayObjectContainer(),
+    renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight),
     santaTexture,
     santa,
+    mincePie,
     santaSpeed = 5,
-    SANTA_SIZE = 250,
-    SANTA_PADDING = 10;
+    santaDirection = DIRECTION.RIGHT;
 
 init();
 
 function init() {
 
-    stage = new PIXI.Stage(0x55813a);
+    stage ;
+
+    // Santa
 
     santaTexture = PIXI.Texture.fromImage('img/santa-mouth-open-250px.png');
 
@@ -19,9 +27,20 @@ function init() {
     santa.position.x = SANTA_PADDING;
     santa.position.y = 50;
 
-    stage.addChild(santa);
+    gameContainer.addChild(santa);
 
-    renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+    // Mince pie
+
+    var mincePieTexture = PIXI.Texture.fromImage('img/mince-pie.png');
+
+    mincePie = new PIXI.Sprite(mincePieTexture);
+
+    mincePie.position.x = window.innerWidth / 2 - (MINCE_PIE_WIDTH/2);
+    mincePie.position.y = window.innerHeight - MINCE_PIE_HEIGHT - 50;
+
+    gameContainer.addChild(mincePie);
+
+    stage.addChild(gameContainer);
 
     document.body.appendChild(renderer.view);
 
@@ -35,9 +54,24 @@ function animate() {
 
     renderer.render(stage);
 
-    if( santa.position.x < window.innerWidth - SANTA_SIZE - SANTA_PADDING ) {
-        santa.position.x += santaSpeed;
+    if( santaDirection == DIRECTION.RIGHT ) {
+
+        if( santa.position.x < window.innerWidth - SANTA_SIZE - SANTA_PADDING ) {
+            santa.position.x += santaSpeed;
+        } else {
+            santaDirection = DIRECTION.LEFT;
+        }
+
+    } else {
+
+        if( santa.position.x > SANTA_PADDING ) {
+            santa.position.x -= santaSpeed;
+        } else {
+            santaDirection = DIRECTION.RIGHT;
+        }
+
     }
+
 
     requestAnimationFrame( animate );
 
@@ -45,8 +79,9 @@ function animate() {
 
 function onResize() {
 
-    // TODO resize and reposition elements too
+    //gameContainer.scale.x = gameContainer.width / window.innerWidth;
+    //gameContainer.height = window.innerHeight;
 
-    renderer.resize(window.innerWidth, window.innerHeight);
+    //renderer.resize(window.innerWidth, window.innerHeight);
 
 }
